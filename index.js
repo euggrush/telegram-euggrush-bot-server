66b4a1d24c079bd417258552eb15240ac9be5014
Evgenii Grushin <evgenii.grushin@fortify.pro>
2023-02-26 01:03:04 -0800

    fix2

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
-import axios from 'axios';
-import {
-    config
-} from 'dotenv';
-import express from 'express';
-import {
-    GoogleSpreadsheet
-} from 'google-spreadsheet';
+import axios from 'axios'
+// import fs from 'fs-extra'
+// import { join } from 'path'
+import { config } from 'dotenv'
+import express from 'express'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
 
-config();
-const app = express();
+config()
+const app = express()
 
-const JOKE_API = 'https://v2.jokeapi.dev/joke/Programming?type=single';
-const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`;
+const JOKE_API = 'https://v2.jokeapi.dev/joke/Programming?type=single'
+const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
 
-app.use(express.json());
-
+app.use(express.json())
 app.use(
     express.urlencoded({
         extended: true
     })
-);
+)
 
-const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
-
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID)
 await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
-});
+})
 
 app.get('/euggrush-tg-bot', (req, res) => {
     res.send('euggrush telegram bot')
 });
 
 app.post('/euggrush-tg-bot', async (req, res) => {
+    const { message } = req.body
 
-    const { message } = req.body;
-    const messageText = message?.text?.toLowerCase()?.trim();
-    const chatId = message?.chat?.id;
+    const messageText = message?.text?.toLowerCase()?.trim()
+    const chatId = message?.chat?.id
     if (!messageText || !chatId) {
         return res.sendStatus(400)
-    };
+    }
 
-    await doc.loadInfo();
-    const sheet = doc.sheetsByIndex[0];
-    const rows = await sheet.getRows();
+    // local json
+    // const dataFromJson = fs.readJSONSync(join(process.cwd(), 'todos.json'))
 
+    // google spreadsheet
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[0]
+    const rows = await sheet.getRows()
     const dataFromSpreadsheet = rows.reduce((obj, row) => {
         if (row.date) {
             const todo = { text: row.text, done: row.done }
             obj[row.date] = obj[row.date] ? [...obj[row.date], todo] : [todo]
         }
         return obj
-    }, {});
+    }, {})
 
-    let responseText = 'I have nothing to say.';
-
+    let responseText = 'I have nothing to say.'
+    // generate responseText
     if (messageText === 'joke') {
         try {
-            const response = await axios(JOKE_API);
-            responseText = response.data.joke;
+            const response = await axios(JOKE_API)
+            responseText = response.data.joke
         } catch (e) {
             console.log(e)
             res.send(e)
         }
     } else if (/\d\d\.\d\d/.test(messageText)) {
+        // responseText = dataFromJson[messageText] || 'You have nothing to do on this day.'
         responseText =
             dataFromSpreadsheet[messageText] || 'You have nothing to do on this day.'
-    };
+    }
 
+    // send response
     try {
         await axios.post(TELEGRAM_URI, {
             chat_id: chatId,
@@ -78,10 +79,9 @@ app.post('/euggrush-tg-bot', async (req, res) => {
         console.log(e)
         res.send(e)
     }
-});
+})
 
-const PORT = process.env.PORT || 3333;
-
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-});
+})
